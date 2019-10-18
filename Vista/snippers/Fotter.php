efa0ff6a045f33fdce7386564b5600643e34b71c
Catalina <yccastro349@misena.edu.co>
2019-10-18 10:34:25 -0500

    :v

--- Vista/snippers/Fotter.php
+++ Vista/snippers/Fotter.php
@@ -2,7 +2,6 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 2.4.0
     </div>
-    <strong>Spring Star &copy; 2019 <a href="https://adminlte.io">Spring Star</a>.</strong> All rights
-    reserved.
+    <strong>Spring Star &copy; 2019</strong>
 </footer>
 
